3464446d6212d10bfdddae20c74a09b51c055756
Josh-Ay <ayo.oloyede16@gmail.com>
2022-06-14 12:36:27 +0100

    Fix linking to login or register page

--- src/component/candidate/Login.js
+++ src/component/candidate/Login.js
@@ -142,7 +142,7 @@ export default function SignIn() {
 							</Link>
 						</Grid>
 						<Grid item>
-							<Link href="/" variant="body2">
+							<Link href="/DowellJobPortal/" variant="body2">
 								{"Don't have an account? Sign Up"}
 							</Link>
 						</Grid>
