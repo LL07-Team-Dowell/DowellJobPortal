d34f51061a60bce20b5e38031910ca3fb390498c
Josh-Ay <ayo.oloyede16@gmail.com>
2022-06-21 01:02:28 +0100

    Update error page

--- src/component/error/ErrorPage.js
+++ src/component/error/ErrorPage.js
@@ -1,5 +1,5 @@
 import Navbar from "../candidate/Navbar";
-import Footer from "../candidate/Footer";
+import { Link } from "react-router-dom";
 
 import "./style.css";
 
@@ -8,11 +8,14 @@ const ErrorPage = () => {
     return <>
         <Navbar />
             
-            <div className="error__Page__Container">
-                <img src={process.env.PUBLIC_URL + "/404-page-not-found.svg"} alt="404 page not found" />
-            </div>
-            
-        <Footer /> 
+        <div className="error__Page__Container">
+            <img src={process.env.PUBLIC_URL + "/404-page-not-found.svg"} alt="404 page not found" />
+        </div>
+
+        <Link to={'home'}>
+            Go Home
+        </Link>
+
     </>
 }
 
