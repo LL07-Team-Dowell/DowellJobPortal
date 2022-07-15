ad1c313a63713e26d61cb2942626391d7a6d652d
Josh-Ay <ayo.oloyede16@gmail.com>
2022-07-16 00:43:28 +0100

    Remove dummy data and show applicant details for hr view

--- src/views/teamlead/components/ApplicationDetails/ApplicationDetails.js
+++ src/views/teamlead/components/ApplicationDetails/ApplicationDetails.js
@@ -1,9 +1,11 @@
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import React from 'react';
+import { excludedApplicantInfo, mutableNewApplicationStateNames } from '../../../../contexts/NewApplicationContext';
 
 import "./style.css";
 
 
-const ApplicantDetails = ({ hrPageActive }) => {
+const ApplicantDetails = ({ hrPageActive, applicantData }) => {
     return <>
         <div className="selected-applicant-details">
             <p>Application details: </p>
@@ -12,15 +14,52 @@ const ApplicantDetails = ({ hrPageActive }) => {
 
         {
             hrPageActive && <div className='selected-applicant-info'>
-                <span>Name</span>
-                <span>Country</span>
-                <span>Email ID</span>
-                <span>Freelance Platform</span>
-                <span>Profile Link of your Freelance</span>
-                <span>Academic Qualification</span>
-                <span>Discord Id</span>
-                <span>Comments, remarks</span>
-                <span>Speed test</span>
+                {
+                    !applicantData ?
+                    <>
+                        <span>Name</span>
+                        <span>Country</span>
+                        <span>Email ID</span>
+                        <span>Freelance Platform</span>
+                        <span>Profile Link of your Freelance</span>
+                        <span>Academic Qualification</span>
+                        <span>Discord Id</span>
+                        <span>Comments, remarks</span>
+                        <span>Speed test</span>
+                    </> : 
+                    <>
+                    {
+                        React.Children.toArray(Object.keys(applicantData || {}).map(key => {
+                            
+                            if ( excludedApplicantInfo.includes(key) || (typeof applicantData[key] === "object") ) return <></>
+
+                            if (typeof applicantData[key] === "object") return <></>
+
+                            if (key === mutableNewApplicationStateNames.applicant) return <span>Name: {applicantData[key]}</span>
+                            if (key === mutableNewApplicationStateNames.country) return <span>Country: {applicantData[key]}</span>
+                            if (key === mutableNewApplicationStateNames.freelancePlatform) return <span>Freelance Platform: {applicantData[key]}</span>
+                            if (key === mutableNewApplicationStateNames.freelancePlatformUrl) return <span>Freelance Platform Url: {applicantData[key]}</span>
+                            
+                            return <span>{key}: {applicantData[key]}</span>
+
+                        }))
+                    }
+
+                    {
+                        React.Children.toArray(Object.keys(applicantData.others || {}).map(key => {
+
+                            if ( excludedApplicantInfo.includes(key) ) return <></>
+
+                            if (key === mutableNewApplicationStateNames.others_property_qualification) return <span>Academic Qualification: {applicantData.others[key]}</span>
+                            if (key === mutableNewApplicationStateNames.others_property_qualification_type) return <span>Qualification Type: {applicantData.others[key]}</span>
+                            if (key === mutableNewApplicationStateNames.others_comments) return <span>Comments: {applicantData.others[key]}</span>
+                            
+                            return <span>{key}: {applicantData.others[key]}</span>
+                        }))
+                    }
+                    </>
+                }
+                
             </div>
         }
     </>
