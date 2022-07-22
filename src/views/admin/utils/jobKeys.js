7fb1608deed98129022ebb189707999b606d9fc0
Josh-Ay <ayo.oloyede16@gmail.com>
2022-07-23 00:28:48 +0100

    Update edit and view job screen in admin page

---
+++ src/views/admin/utils/jobKeys.js
@@ -0,0 +1,13 @@
+export const jobKeys = {
+    jobTitle: "title",
+    jobDescription: "description",
+    skillsRequired: "skills",
+    jobIsActive: "is_active",
+    jobType: "typeof",
+    jobTimePeriod: "time_period",
+    generalTerms: "general_terms",
+    technicalSpecifications: "Technical_Specifications",
+    paymentTerms: "Payment_terms",
+    workflowOfJob: "workflow",
+    others: "others",
+}
