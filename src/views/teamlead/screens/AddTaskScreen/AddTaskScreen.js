d3e7f8dc34ec2b2ac85e0147945d40c6ea6e09bf
Josh-Ay <ayo.oloyede16@gmail.com>
2022-07-27 21:35:42 +0100

    Add task functionality to teamlead view

---
+++ src/views/teamlead/screens/AddTaskScreen/AddTaskScreen.js
@@ -0,0 +1,115 @@
+import React, { useEffect, useRef, useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
+import useClickOutside from "../../../../hooks/useClickOutside";
+import { IoIosArrowBack } from "react-icons/io";
+
+import "./style.css";
+import { myAxiosInstance } from "../../../../lib/axios";
+import { routes } from "../../../../lib/routes";
+import { useNavigate } from "react-router-dom";
+
+
+const AddTaskScreen = ({ teamMembers , closeTaskScreen }) => {
+
+    const ref = useRef(null);
+    const [ showTaskForm, setShowTaskForm ] = useState(false);
+    const [ newTaskDetails, setNewTaskDetails ] = useState({
+        "username": "",
+        "title": "",
+        "description": "",
+        "status": "Incomplete",
+    });
+    const [ disabled, setDisabled ] = useState(true);
+    const navigate = useNavigate();
+
+    useClickOutside(ref, () => closeTaskScreen());
+
+    useEffect (() => {
+
+        if (newTaskDetails.username.length < 1) return setShowTaskForm(false);
+
+        if ((newTaskDetails.title.length < 1) || (newTaskDetails.description.length < 1)) return setDisabled(true)
+        
+        setDisabled(false)
+
+    }, [newTaskDetails])
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setNewTaskDetails(prevValue => { return { ...prevValue, [name]: value } })
+    }
+
+    const handleMemberItemClick = (member) => {
+        setNewTaskDetails(prevValue => { return { ...prevValue, "username": member }});
+        setShowTaskForm(true); 
+    }
+
+    const handleNewTaskBtnClick = async () => {
+
+        setDisabled(true);
+
+        const dataToSend = { ...newTaskDetails };
+        dataToSend.user = newTaskDetails.username;
+        delete dataToSend["username"];
+
+        try{
+
+            await myAxiosInstance.post(routes.Add_New_Task, dataToSend);
+            // update tasks [], show task screen
+            closeTaskScreen();
+            navigate("/tasks");
+
+        } catch (err) {
+            console.log(err);
+            setDisabled(false);
+        }
+
+    }
+
+    console.log(teamMembers)
+
+    return <>
+        <div className="add__New__Task__Overlay">
+            <div className="add__New__Task__Container" ref={ref}>
+                <h1 className="title__Item">
+                    {
+                        showTaskForm ? <>
+                            <IoIosArrowBack onClick={() => setShowTaskForm(false)} style={{ cursor: "pointer" }} />
+                            New Task Details
+                        </> : <>Add new task</>
+                    }
+
+                    <AiOutlineClose onClick={() => closeTaskScreen()} style={{ cursor: "pointer" }} />
+                </h1>
+                {
+                    showTaskForm ? <>
+                        <input type={"text"} placeholder={"Task Assignee"} value={newTaskDetails.username} readOnly={true} />
+                        <input type={"text"} placeholder={"Task Title"} name="title" value={newTaskDetails.title} onChange={handleChange} />
+                        <textarea placeholder="Task Description" name="description" value={newTaskDetails.description} onChange={handleChange}></textarea>
+                        <button type={"button"} className="add__Task__Btn" disabled={disabled} onClick={handleNewTaskBtnClick}>Add Task</button>
+                    </> :
+                    
+                    <>
+                        {
+                            teamMembers.length < 1 ? <>
+                                <h4>Your team members will appear here</h4>
+                            </> :
+                            <>
+                                <h4>Your team members ({teamMembers.length})</h4>
+                                <div className="team__Members__Container">
+                                    {
+                                        React.Children.toArray(teamMembers.map(member => {
+                                            return  <p className="team__Member__Item" onClick={() => handleMemberItemClick(member.applicant)}>{member.applicant}</p>
+                                        }))
+                                    }
+                                </div>
+                            </>
+                        }
+                    </>
+                }
+            </div>
+        </div>
+    </>
+}
+
+export default AddTaskScreen;
