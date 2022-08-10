7fbec3e9c8765a966405303c2ce5818953ff5247
Josh-Ay <ayo.oloyede16@gmail.com>
2022-08-10 07:01:23 +0100

    Remove testing code

--- src/views/admin/components/JobDetails/JobDetails.js
+++ src/views/admin/components/JobDetails/JobDetails.js
@@ -57,7 +57,7 @@ const JobDetails = ({ currentJob, editPage, updateJobDetails }) => {
         updateOptions(jobKeys.others);
 
     }, [currentJob[jobKeys.generalTerms], currentJob[jobKeys.workflowOfJob], currentJob[jobKeys.technicalSpecifications], currentJob[jobKeys.paymentTerms]])
-    
+
     
     return <>
         <div className="current__Job__Display">
@@ -225,46 +225,6 @@ const JobDetails = ({ currentJob, editPage, updateJobDetails }) => {
                 </>
             }
 
-            {/* {
-                React.Children.toArray(Object.keys(currentJob || {}).map(key => {
-                    
-                    if (typeof currentJob[key] === "object") return <></>
-
-                    if (key === "id") return <></>
-
-                    if (key === "title") return <span className={`title__Text ${editPage ? 'display__Flex' : ''}`}>
-                        {
-                            editPage ? 
-                            <> <b>{key[0].toLocaleUpperCase() + key.slice(1).toLocaleLowerCase()}</b>: <input type={"text"} name={key} value={currentJob[key]} onChange={handleChange} /> </>
-                            :  
-                            <><b>{currentJob[key]}</b><p>Dowell Ux living lab</p><CustomHr /> </>
-                        }
-                    </span>
-
-                    if (key === "skills") return <>
-                        {
-                            editPage ? <></> :
-                            <span>Skills: {currentJob[key]}</span>
-                        }
-                    </>
-
-                    if (key === "description") return <>
-                        <span className="description__Text"><b>Job {key}</b></span>
-                        <textarea value={currentJob[key]} readOnly={editPage ? false : true} rows={3} onChange={handleChange} name={key}></textarea>
-                    </>
-
-                    if ((key === "created") || (key === "updated")) return <span>{key[0].toLocaleUpperCase() + key.slice(1).toLocaleLowerCase()}: {formatDateAndTime(currentJob[key])}</span>
-
-                    if (key === "typeof") return <span className={`${editPage ? 'display__Flex' : ''}`}>{key[0].toLocaleUpperCase() + key.slice(1).toLocaleLowerCase()}: {editPage ? <DropdownButton currentSelection={currentJob[key]} selections={["Freelance", "Employee", "Internship"]} handleSelectionClick={(selection) => updateJobDetails(prevValue => { return { ...prevValue, [key]: selection } })} /> : currentJob[key]}</span>
-                    
-                    if (key === "is_active") return <span className={`${editPage ? 'display__Flex' : ''}`}>{key[0].toLocaleUpperCase() + key.slice(1).toLocaleLowerCase()}: {editPage ? <DropdownButton currentSelection={currentJob[key] === true ? "true" : "false"} selections={["true", "false"]} handleSelectionClick={(selection) => updateJobDetails(prevValue => { return { ...prevValue, [key]: selection } })} /> : currentJob[key] === true ? "true" : "false"}</span>
-                    
-                    if (key === "time_period") return <span className={`${editPage ? 'display__Flex' : ''}`}>{key[0].toLocaleUpperCase() + key.slice(1).toLocaleLowerCase()}: {editPage ? <DropdownButton currentSelection={currentJob[key]} selections={["Less than 1 month", "1-3 Months", "3-6 Months", "More than 6 Months"]} handleSelectionClick={(selection) => updateJobDetails(prevValue => { return { ...prevValue, [key]: selection } })} /> : currentJob[key]}</span>
-
-                    return <span className={`${editPage ? 'display__Flex' : ''}`}>{key[0].toLocaleUpperCase() + key.slice(1).toLocaleLowerCase()}: {editPage ? <input type={"text"} name={key} value={currentJob[key]} onChange={handleChange} /> : currentJob[key]}</span>
-                    
-                }))
-            } */}
         </div>
     </>
 
