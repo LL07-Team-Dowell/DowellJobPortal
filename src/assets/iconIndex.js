9f35690492b1ad007a12803971d9f0ce0bc439d7
Josh-Ay <ayo.oloyede16@gmail.com>
2022-08-23 10:30:19 +0100

    Done with redesign of landing page

---
+++ src/assets/iconIndex.js
@@ -0,0 +1,12 @@
+import python from "./job-icons/python-programmer.svg";
+import social from "./job-icons/social-media.svg";
+import ui from "./job-icons/ui-ux.svg";
+import assistant from "./job-icons/virtual-assistant.svg";
+
+
+export {
+    python,
+    social,
+    ui,
+    assistant,
+}
